228e78f5998dcb59ca88f2545a6d21f15e6d7bc1
Denis Maximenko <maximenko@owlcat.games>
2024-08-18 17:32:01 +0400

    Implement projectile lifetime

--- Shooting/projectile.gd
+++ Shooting/projectile.gd
@@ -5,6 +5,7 @@ extends RigidBody2D
 
 func _ready() -> void:
 	linear_velocity = Vector2.RIGHT * config.speed
+	%DeathTimer.start(config.projectile_lifetime)
 	pass
 
 func _physics_process(delta: float) -> void:
