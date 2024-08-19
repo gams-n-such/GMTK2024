25d8cec2b41776b7de855f3661af87f04ad5a7f6
Z0RIK <113785827+Z0RIK@users.noreply.github.com>
2024-08-20 00:14:25 +0600

    added placeholder art

--- Arenas/Test/test_arena.gd
+++ Arenas/Test/test_arena.gd
@@ -9,9 +9,9 @@ func _input(event):
 		#body.paused = true
 		for i in range(3):
 			city_blocks.push_back(preload("res://Cities/city_block.tscn").instantiate())
-		city_blocks[0].get_node("CityBlockArtPlaceholder").texture = preload("res://testing/city_block_art_placeholder_red.png")
-		city_blocks[1].get_node("CityBlockArtPlaceholder").texture = preload("res://testing/city_block_art_placeholder_green.png")
-		city_blocks[2].get_node("CityBlockArtPlaceholder").texture = preload("res://testing/city_block_art_placeholder_blue.png")
+		city_blocks[0].get_node("CityBlockArtPlaceholder").texture = preload("res://cities/city_block_art_placeholder_red.png")
+		city_blocks[1].get_node("CityBlockArtPlaceholder").texture = preload("res://cities/city_block_art_placeholder_green.png")
+		city_blocks[2].get_node("CityBlockArtPlaceholder").texture = preload("res://cities/city_block_art_placeholder_blue.png")
 		city_builder = preload("res://Cities/city_builder.tscn").instantiate()
 		city_builder.setup(city_blocks, city)
 		self.add_child(city_builder)
