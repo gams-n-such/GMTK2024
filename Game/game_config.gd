a6a5282d76da48423058126fa74c02f41e28c8ce
Denis Maximenko <maximenko@owlcat.games>
2024-08-20 12:37:49 +0400

    Fix proper upgrade options generation

--- Game/game_config.gd
+++ Game/game_config.gd
@@ -4,3 +4,5 @@ extends Resource
 # TODO: store waves, each wave has array of mobs with spawn chances and its start/end times
 
 @export var progression_settings : ProgressionConfig
+
+@export var upgrade_options_chances : UpgradeOptionsPool
